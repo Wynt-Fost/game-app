01906e23284ce48a8db57e2724d3ace038d4cbec
Akeem <akeemfoster2@gmail.com>
2022-01-18 17:48:44 -0500

    adding first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import displayGame from './components/displayGame.js'
+
 
 function App() {
+  const apiKey = 'd07f35abd3f241b28dd811332c24aff7';
+
+  const [game, setGame] = useState([])
+
+  const [userInput, setUserInput] = useState('');
+  const [gameSearch, setGameSearch] = useState('');
+
+  useEffect(() => {
+    axios({
+      url: 'https://api.rawg.io/api/games',
+      // url: 'https://api.rawg.io/api/games/{id}',
+      method: "GET",
+      dataResponse: 'json',
+      params: {
+        key: apiKey,
+        // search: gameSearch,
+        search: 'escape from tarkov',
+        // id: 52106,
+        page_size: 1
+        // q: searchTerm
+      }
+    }).then((response) => {
+      console.log(response.data.results)
+
+      setGame(response.data.results)
+
+    })
+  }, [gameSearch]);
+
+  const takeInput = (event) => {
+    // console.log('is this working?', event.target.value)
+    setUserInput(event.target.value);
+
+  }
+
+
+  const userSubmit = (event) => {
+    event.preventDefault();
+    setGameSearch(userInput);
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Choose your game!</h1>
+
+      {<form onSubmit={userSubmit}>
+        <label htmlFor="search">Search for your favourite game: </label>
+        <input type="text" id='search' onChange={takeInput} value={userInput} />
+        <button>Search</button>
+      </form>}
+
+      {/* <displayGame theGame={theGame} /> */}
+
+      {game.map((gameInfo) => {
+        return (
+          <div>
+            <h2>{gameInfo.name}</h2>
+            <p>{gameInfo.metacritic}</p>
+            <img src={gameInfo.background_image} alt="" />
+          </div>
+        )
+      })}
+
     </div>
   );
 }
---
+++ src/components/displayGame.js
@@ -0,0 +1,22 @@
+
+
+// const displayGame = ({ theGame }) => {
+//     return (
+//         <section>
+//             {
+//                 game.map((gameInfo) => {
+//                     return (
+//                         <div>
+//                             <h2>{gameInfo.name}</h2>
+//                             <p>{gameInfo.metacritic}</p>
+//                             <img src={gameInfo.background_image} alt="" />
+//                         </div>
+//                     )
+//                 })
+//             }
+
+//         </section>
+//     )
+// }
+
+// export default displayGame
