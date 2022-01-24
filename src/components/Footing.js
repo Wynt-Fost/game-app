e1f33f84e8bcfcfa54c102fe7b17c267453dadf1
Akeem Foster <68609654+Wynt-Fost@users.noreply.github.com>
2022-01-24 12:22:54 -0500

    Revert "Merging my main to production"

--- src/components/Footing.js
+++
@@ -1,9 +0,0 @@
-const footing = (props) => {
-    return (
-        <footer>
-            Created by Akeem Foster @  <a href="https://junocollege.com/">Juno College</a>
-        </footer>
-    )
-}
-
-export default footing
